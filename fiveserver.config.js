0ae50ab430d237f2629e5070491ed1ac7575a39c
Reyu Hero <reyu443@gmail.com>
2022-03-21 03:47:50 +0430

    profile freelancer created

--- fiveserver.config.js
+++ fiveserver.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-  php: "C:\\laragon\\bin\\php\\php-7.4.16-nts-Win32-vc15-x64\\php.exe"   // Windows
+  php: "C:\\laragon\\bin\\php\\php-7.4.16-nts-Win32-vc15-x64\\php.exe",  // Windows
+  highlight: true, // enable highlight feature
+  injectBody: true, // enable instant update
+  remoteLogs: true, // enable remoteLogs
+  remoteLogs: "yellow", // enable remoteLogs and use the color yellow
+  injectCss: false, // disable injecting css
+  navigate: true, // enable auto-navigation
+  debugVSCode: true,
 }
