5076f2decb112a4b4abb668e5e76e1574bd63fb3
Reyu Hero <reyu443@gmail.com>
2022-04-06 07:10:46 +0430

    team-create-talent-recruitment-post

--- import.mjs
+++ import.mjs
@@ -1,5 +1,17 @@
-customElements.define('my-component', class extends HTMLElement {
-    connectedCallback() {
-        this.innerHTML = `<b>Reza Yousofi</b>`;
+class MyElement extends HTMLElement{
+    constructor(){
+        super();
+        this.attachShadow({mode: "open"})
+        this.render();
     }
-});
+    render(){
+        this.shadowRoot.innerHTML = //html
+        `
+        <style>
+            @import url('style.css');
+        </style>
+        <h2 class="text-danger h2">Hello Ryo</h2>
+        `
+    }
+}
+customElements.define("my-el",MyElement);
